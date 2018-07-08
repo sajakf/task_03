234a3ec6559b1ffcf9bd89b1d71eff715b2cd174
SKF <skf@SKFs-MacBook-Pro.local>
2018-07-08 16:13:01 +0300

    Task_03

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    	"my_list": [
+            {
+                "restaurant_name":"Burgers",
+                "Food_type":"American",
+            },
+            {
+                "restaurant_name":"Pizza",
+                "Food_type":"Italian",
+            },
+            {
+                "restaurant_name":"Taccos",
+                "Food_type":"Mexican",
+            },
+        ],
 
     }
     return render(request, 'list.html', context)
@@ -13,7 +27,7 @@ def restaurant_list(request):
 
 def restaurant_detail(request):
 
-    context = {
+    context = {'restaurant_name' : 'Lorenzo'
 
     }
     return render(request, 'detail.html', context)
